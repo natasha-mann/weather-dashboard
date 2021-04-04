b391f446f0db4315c44bca356e52b1a735564872
Natasha Mann <75756325+natasha-mann@users.noreply.github.com>
2021-04-04 21:26:26 +0100

    added object for current day forecast data

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,21 +38,71 @@ const getFromLocalStorage = () => {
   }
 };
 
+const getCurrentDayWeather = (oneApiData) => {
+  return {
+    date: dataFromServer.current.dt,
+    iconURL: `http://openweathermap.org/img/wn/${dataFromServer.current.weather[0].icon}@2x.png`,
+    temperature: dataFromServer.current.temp,
+    humidity: dataFromServer.current.humidity,
+    windSpeed: dataFromServer.current.wind_speed,
+    uvIndex: dataFromServer.current.uvi,
+  };
+};
+
+const getForecastData = (oneApiData) => {
+  // iterate and construct the return data array
+  return [
+    {
+      date: "",
+      iconURL: "",
+      temperature: "",
+      humidity: "",
+    },
+  ];
+};
+
 const fetchWeatherData = (cityName) => {
   const weatherApiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=785940357963f0488e126bd41a8d1e5c`;
 
-  const functionForJSON = (responseObject) => {
-    return responseObject.json();
+  const functionForJSON = (currentDayData) => {
+    return currentDayData.json();
   };
 
-  const functionForApplication = (dataFromServer) => {
-    console.log(dataFromServer);
-    const cityLonLat = dataFromServer.coord;
+  const functionForApplication = (currentDayData) => {
+    const cityLonLat = currentDayData.coord;
     const oneApiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${cityLonLat.lat}&lon=${cityLonLat.lon}&appid=785940357963f0488e126bd41a8d1e5c`;
+
+    const functionForJSON = (oneApiData) => {
+      return oneApiData.json();
+    };
+
+    const functionForApplication = (oneApiData) => {
+      console.log(oneApiData);
+      const currentDayData = getCurrentDayWeather(oneApiData);
+      console.log(currentDayData);
+      const forecastData = getForecastData();
+      // renderCurrentCardComponent(currentData);
+      // renderForecastCardComponent(forecastData);
+    };
+
+    const functionToHandleError = (errorObject) => {
+      // handle your error here according to your application
+    };
+
+    fetch(oneApiUrl)
+      .then(functionForJSON)
+      .then(functionForApplication)
+      .catch(functionToHandleError);
   };
 
-  fetch(weatherApiUrl).then(functionForJSON).then(functionForApplication);
-  // .catch(functionToHandleError);
+  const functionToHandleError = (errorObject) => {
+    // handle your error here according to your application
+  };
+
+  fetch(weatherApiUrl)
+    .then(functionForJSON)
+    .then(functionForApplication)
+    .catch(functionToHandleError);
 };
 
 // function called on load of the document
@@ -62,7 +112,7 @@ const onLoad = () => {
     renderCities(citiesFromLocalStorage);
   }
   // get the last city name from citiesFromLocalStorage and store in variable called cityName
-  // fetchAllWeatherData(cityName)
+  // fetchWeatherData(cityName);
 };
 
 $("#cityForm").submit(onSubmit);
